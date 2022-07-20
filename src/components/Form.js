adad335bcd465aa84e7f734c9df6daa0a81998c0
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-07-20 22:35:53 +0300

    commit tree 66d3ea7229815231457d52e5a92787f843c14f72

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,12 +12,63 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (firstName.length > 0) {
+      const formData = {
+        firstName: firstName,
+        lastName: lastName,
+      };
+      const dataAsArray = [...submittedData, formData];
+      setSubmittedData(dataAsArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required"]);
+    }
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
+
   return (
     <form>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+     function handleSubmit(event) {
+      event.preventDefault();
+      if (firstName.length > 0) {
+        const formData = {
+          firstName: firstName,
+          lastName: lastName,
+        };
+        const dataAsArray = [...submittedData, formData];
+        setSubmittedData(dataAsArray);
+        setFirstName("");
+        setLastName("");
+        setErrors([]);
+      } else {
+        setErrors(["First name is required"]);
+      }
+    }
+  
+    const listOfSubmissions = submittedData.map((data, index) => {
+      return (
+        <div key={index}>
+          {data.firstName} {data.lastName}
+        </div>
+      );
+    });
+  
   );
 }
 
